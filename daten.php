53327c80282877fd47945409d9eba5da612988dc
reiniaust <55030087+reiniaust@users.noreply.github.com>
2019-10-04 18:22:44 +0200

    Update daten.php

--- daten.php
+++ daten.php
@@ -3,21 +3,46 @@
 	
 	$ordner = "/var/www/vhosts/reinhard-austermeier.de/httpdocs/~upload/";
 
-	if ($_GET["aktion"] == "lesen") {
-		if ($_GET["username"] == "reini" && $_GET["passwort"] == "Sces04ok!") {
-			$datei = $ordner . $_GET["datei"];
+	
+	$benutzer = array(
+		"reini" => array(
+			"passwort" => "Sces04ok!",
+			"gruppen" => array(
+				"Reini-privat",
+				"sd-software",
+				"Schützenvorstand"
+			)
+		)
+	);
 
-			$fp = fopen($datei, "r");
-			$ausgabe = fread($fp, filesize($datei)-1);
-			fclose($fp);
-			$fileList = glob($ordner . "infos-*.json");
-			$ausgabe .= ', "dateien": [';
-			foreach($fileList as $filename){
-				$ausgabe .= '"' . substr($filename, strlen($ordner)) . '",';
-			}
-			$ausgabe = substr($ausgabe, 0, strlen($ausgabe)-1);
-			$ausgabe .= ']}';
-			echo $ausgabe;
+	if ($_GET["aktion"] == "lesen") {
+		$user = $benutzer[$_GET["username"]];
+		if ($user["passwort"] == $_GET["passwort"]) {
+			//foreach ($gruppen as $key => $gruppe) {
+				$datei = $ordner . $_GET["datei"];
+				//$datei = $ordner . $gruppe;
+	
+				$fp = fopen($datei, "r");
+				$ausgabe = fread($fp, filesize($datei)-1);
+				fclose($fp);
+
+				$fileList = glob($ordner . "infos-*.json");
+				$ausgabe .= ', "dateien": [';
+				foreach($fileList as $filename){
+					$ausgabe .= '"' . substr($filename, strlen($ordner)) . '",';
+				}
+				$ausgabe = substr($ausgabe, 0, strlen($ausgabe)-1) . "]";
+				
+				$ausgabe .= ', "gruppen": [';
+				foreach ($user["gruppen"] as $key => $gruppe) {
+					$ausgabe .= '"' . $gruppe . '",';
+				}
+				$ausgabe = substr($ausgabe, 0, strlen($ausgabe)-1) . "]";
+				
+				$ausgabe .= '}';
+				
+				echo $ausgabe;
+			//}
 		} else {
 			echo '{"Fehler": "Anmeldung"}';
 		}
